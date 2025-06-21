1a8bb84d02e2ccf09c44b4e04d4f7164272b0ca6
Abhishek verma <abhiver9027@gmail.com>
2025-06-21 17:19:53 +0000

    datatype

---
+++ 01-basic/02_datatype.js
@@ -0,0 +1,33 @@
+"use strict";// treat all js code as newer version
+// alert(3+3)// we are using nodejs, not browser
+console.log(3+3);
+console.log("hitesh");
+
+
+
+
+
+
+let age = 19
+let isLogeedIn = false
+let state; 
+
+
+
+/*
+number => 2 to power53
+bigInt
+string = ""
+boolean => standalone value 
+undefined
+symbol => unique
+
+
+
+
+object 
+
+*/
+//console.log(typeof"hitesh")
+//console.log(typeof"null") object
+//console.log(typeof"undefined")undefined
