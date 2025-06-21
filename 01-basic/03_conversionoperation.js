f248860d4ece91ef8201aa41ab0ddc01251aa9fb
Abhishek verma <abhiver9027@gmail.com>
2025-06-21 17:42:17 +0000

    conversionoperation

---
+++ 01-basic/03_conversionoperation.js
@@ -0,0 +1,19 @@
+//let score = 33 
+
+//console.log(typeof(score));
+//console.log(typeof(33));
+//let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber))
+//console.log(valueInNumber)
+// "33" = 33
+// "33abc" = nan nnumber an number
+// true = 1 ; false = 0
+
+//let isLoggedIn = 1
+//let booleanIsLoggedIn = boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+let someNumber = 22
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
